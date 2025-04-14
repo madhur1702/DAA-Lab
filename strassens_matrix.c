ae2aa6e8f93b3c5ee562e77c16fa7e1aca9b6ec9
Madhur Bhandarkar <150015788+madhur1702@users.noreply.github.com>
2025-04-14 13:42:43 +0530

    Update strassens_matrix.c

--- strassens_matrix.c
+++ strassens_matrix.c
@@ -5,7 +5,7 @@ int a[2][2], b[2][2], c[2][2], i, j;
 int m1, m2, m3, m4 , m5, m6, m7; 
 printf("Enter the 4 elements of first matrix: "); 
 for(i = 0;i < 2; i++) 
-for(j = 0;j < 2; j++) 
+for(j = 0;j < 2; j++)
 scanf("%d", &a[i][j]); 
 printf("Enter the 4 elements of second matrix: "); 
   for(i = 0; i < 2; i++) 
